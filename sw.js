f4a4a4fab55f2d6e7f7206a2e26c6a5c3e00ae34
CharlotteFTSY <bismarkdrei@gmail.comecho>
2024-04-28 00:49:37 +0800

    test

--- sw.js
+++ sw.js
@@ -2,28 +2,17 @@
 
 const CACHE_NAME = 'offline-cache-v1';
 const urlsToCache = [
-<<<<<<< HEAD
   '/Timer/index.html',
   '/Timer/sw.js',
   '/Timer/',
 ];
 
 
-=======
-  '/Timer/',
-  '/Timer/index.html',
-  '/Timer/sw.js',
-];
-
->>>>>>> origin/main
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-<<<<<<< HEAD
       .then(() => self.skipWaiting()) // 跳过等待状态，立即激活新的 Service Worker
-=======
->>>>>>> origin/main
   );
 });
 
