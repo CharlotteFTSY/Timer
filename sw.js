cb249d9f1a5e3dbcaa2e6ec660ed7aa1d20b5bd3
CharlotteFTSY <155338434+CharlotteFTSY@users.noreply.github.com>
2024-04-28 16:26:35 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting()) // 跳过等待状态，立即激活新的 Service Worker
+      .then(() => self.skipWaiting())
   );
 });
 
@@ -17,7 +17,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request)
       .then(response => {
-        // 检查是否成功获取到资源，如果成功则将资源添加到缓存中并返回给页面
         if (!response || response.status !== 200 || response.type !== 'basic') {
           return response;
         }
@@ -31,13 +30,21 @@ self.addEventListener('fetch', event => {
         return response;
       })
       .catch(() => {
-        // 如果网络请求失败，则从缓存中返回匹配的资源
         return caches.match(event.request);
       })
   );
 });
 
-// 当新的 Service Worker 安装完成后，立即激活新的 Service Worker
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
